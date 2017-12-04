840b65fab6399900731e41e9de0cbe9582c5158a
Alexandra Leisse <troubalex@gmail.com>
2017-12-04 14:52:27 +0100

    Responsive navigation :v::sunglasses:

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,14 @@
-// This is where it all goes :)
+let overlay = document.getElementsByClassName('overlay').item(0);
+let menuToggle = document.getElementsByClassName('menu-trigger').item(0);
+let closeButton = document.getElementsByClassName('close-overlay').item(0);
+
+function toggleMenu() {
+    overlay.classList.toggle('open');
+}
+
+menuToggle.onclick = function() {
+    toggleMenu();
+}
+closeButton.onclick = function() {
+    toggleMenu();
+}
