956198330a14c8da83012b3c5f45cd91d425f13e
Alexandra Leisse <troubalex@gmail.com>
2017-12-21 13:10:28 +0100

    Adds new loading animation, alpha version

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -12,3 +12,15 @@ menuToggle.onclick = function() {
 closeButton.onclick = function() {
     toggleMenu();
 }
+
+
+function setAnimationProperties() {
+    let path = document.getElementById('symbol-outline').querySelector('path');
+    let pathLength = path.getTotalLength();
+    let duration = (Math.ceil(pathLength) / 1000 * 5).toString() + 's';
+    console.log(duration);
+    
+    path.style.setProperty('--path-length', pathLength);
+    path.style.setProperty('--duration', duration);
+}
+setAnimationProperties();
