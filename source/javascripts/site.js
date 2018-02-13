7c2bf83ca4ae3a877c0819829114394ac13fe958
Alexandra Leisse <troubalex@gmail.com>
2018-02-13 18:05:14 +0100

    Removed pricing toggle for now

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -13,26 +13,6 @@ closeButton.onclick = function() {
     toggleMenu();
 }
 
-// TOGGLE FOR SUBSCRIPTION PLANS (MONTH VS YEAR)
-let billingPeriodToggleMonth = document.getElementsByClassName('billing-period-toggle').item(0);
-let billingPeriodToggleYear = document.getElementsByClassName('billing-period-toggle').item(1);
-
-function toggleBillingPeriodMonth () {
-    billingPeriodToggleMonth.classList.add('selected');
-    billingPeriodToggleYear.classList.remove('selected');
-}
-function toggleBillingPeriodYear () {
-    billingPeriodToggleYear.classList.add('selected');
-    billingPeriodToggleMonth.classList.remove('selected');
-}
-
-billingPeriodToggleMonth.onclick = function() {
-    toggleBillingPeriodMonth();
-}
-billingPeriodToggleYear.onclick = function() {
-    toggleBillingPeriodYear();
-}
-
 // SETTING VALUES ON CSS VARIABLES FOR THE LOADER ANIMATION
 // function setAnimationProperties() {
 //     let path = document.getElementById('symbol-outline').querySelector('path');
