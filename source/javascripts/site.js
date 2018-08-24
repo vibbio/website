8a678a17e8b28eee60fb0f3f001fbcaede4a79ff
Alexandra Leisse <troubalex@gmail.com>
2018-08-24 22:13:01 +0200

    CONFETTI! :tada:

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -13,6 +13,22 @@ closeButton.onclick = function() {
     toggleMenu();
 }
 
+var canvasWidth = document.getElementById('videoExamples').offsetWidth;
+var canvasHeight = document.getElementById('videoExamples').offsetHeight;
+
+var confettiSettings = {
+        target: 'my-canvas',
+        max: '30',
+        animate: false,
+        props: ["circle","square","line"],
+        colors: [[63,207,213],[35,55,70],[0,131,155],[0,88,111]],
+        clock: '25',
+        width: canvasWidth,
+        height: canvasHeight
+};
+var confetti = new ConfettiGenerator(confettiSettings);
+confetti.render();
+
 // SETTING VALUES ON CSS VARIABLES FOR THE LOADER ANIMATION
 // function setAnimationProperties() {
 //     let path = document.getElementById('symbol-outline').querySelector('path');
